c03df4ac2f4e47ddd078bbccb123a13190355018
ernesg93 <ernesg93@gmail.com>
2026-03-29 15:26:17 -0400

    feat: agregar paginacion al catalogo

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.staticfiles import finders
 
@@ -30,7 +31,18 @@ def lista_juegos(request):
         else:
             juego.portada_static_path = default_portada
 
+    paginator = Paginator(juegos, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    current_page = page_obj.number
+    page_range_start = max(current_page - 2, 1)
+    page_range_end = min(current_page + 2, paginator.num_pages)
+    visible_pages = range(page_range_start, page_range_end + 1)
+
     context = {
-        "lista_juegos": juegos,
+        "lista_juegos": page_obj,
+        "page_obj": page_obj,
+        "visible_pages": visible_pages,
     }
     return render(request, "catalogo/lista_juegos.html", context)
